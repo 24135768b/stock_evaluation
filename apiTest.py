2e8d8650e04cf196fdad8ad26cc289b2d9f25036
tsai <24135768b@gmail.com>
2024-11-24 15:11:46 +0800

    feat: calculate value of a stock

--- apiTest.py
+++ apiTest.py
@@ -1,26 +1,68 @@
+import random
 import requests
 
-url = "https://www.tradingvalley.tw/api/stock-mining/symbol-info/NVDA"
+def get_stock_values(stock_symbol: str) -> dict: 
 
-headers = {
-    "accept": "*/*",
-    "accept-language": "en",
-    "priority": "u=1, i",
-    "sec-ch-ua": "\"Google Chrome\";v=\"131\", \"Chromium\";v=\"131\", \"Not_A Brand\";v=\"24\"",
-    "sec-ch-ua-mobile": "?0",
-    "sec-ch-ua-platform": "\"Windows\"",
-    "sec-fetch-dest": "empty",
-    "sec-fetch-mode": "cors",
-    "sec-fetch-site": "cross-site",
-    "Referer": "https://www.growin.tv/",
-    "Referrer-Policy": "strict-origin-when-cross-origin"
-}
+    url = "https://www.tradingvalley.tw/api/stock-mining/symbol-info/" + stock_symbol
 
-response = requests.get(url, headers=headers)
+    headers = {
+        "accept": "*/*",
+        "accept-language": "en",
+        "priority": "u=1, i",
+        "sec-ch-ua": "\"Google Chrome\";v=\"131\", \"Chromium\";v=\"131\", \"Not_A Brand\";v=\"24\"",
+        "sec-ch-ua-mobile": "?0",
+        "sec-ch-ua-platform": "\"Windows\"",
+        "sec-fetch-dest": "empty",
+        "sec-fetch-mode": "cors",
+        "sec-fetch-site": "cross-site",
+        "Referer": "https://www.growin.tv/",
+        "Referrer-Policy": "strict-origin-when-cross-origin"
+    }
 
-print(response.status_code)
-print(response.json())  # Assuming the response is in JSON format
+    response = requests.get(url, headers=headers)
 
-data = response.json()['data']
-print("value: ", data['value'])
-print("trend: ", data['trend'])
+    # print(response.status_code)
+    # print(response.json())  # Assuming the response is in JSON format
+
+    data = response.json()['data']
+
+    if(data["isExist"]):
+        keys = ['value', 'trend', 'swing', 'dividend']
+        return {key: data[key] for key in keys}
+    else:
+        return {}
+
+
+def get_all_tickets():
+    # Example with Alpha Vantage (replace with your API key)
+    api_key = "CWHPD0K0I6IDKJOR"
+    url = f"https://www.alphavantage.co/query?function=LISTING_STATUS&apikey={api_key}"
+    response = requests.get(url)
+
+    if response.status_code == 200:
+        stock_data = response.text
+        # Process stock_data to extract tickers (e.g., CSV parsing)
+        # Split into lines and extract the symbols
+        lines = stock_data.strip().split("\n")  # Split by newline
+        symbols = [line.split(",")[0] for line in lines[1:]]  # Skip header and take the first column
+        
+        # print(symbols) # Print
+        # print("numbers: " + str(len(symbols)))
+        return symbols
+    else:
+        print("Failed to fetch data:", response.status_code)
+
+    
+# all_tickets:list = get_all_tickets()
+# random_stocks:list = random.sample(all_tickets, 5)
+# print(random_stocks)  
+
+# for stock in random_stocks:
+#     stock_values: dict = get_stock_values(stock)
+#     print(stock_values)
+stock_values: dict = get_stock_values('NVDA')
+print(stock_values)
+stock_values: dict = get_stock_values('AOD')
+print(stock_values)
+if(stock_values):
+    print("ddd")
